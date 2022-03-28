2c1ecc6d8d37c1de9d93a64f9384a1779b9e804b
Rachel Collins <spiritedndiligent@gmail.com>
2022-03-27 21:37:41 -0400

    cleaning up

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import '../style.css';
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App';
-
-   //fetch and DOM manip here
-// console.log('gifholder ', gifHolder[0])
-// gifHolder.append('test this')
-   //fetch and DOM manip here
-
  
 ReactDOM.render(<App />, document.getElementById('root'));
