a91645ba730c9270f2c48d46586c9e3276f9de68
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-09 21:36:11 -0500

    script updates

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ console.log('activeSlide', buttons)
 buttons.forEach( (button, i) => {
     button.addEventListener('click', () => {
         button.dataset.carouselButton === 'prev' ? 1 : -1;
-        const slides = button.closest("[data-carousel]")
+        const slides = button.closest('[data-carousel]')
+
         .querySelector('[data-slides]');
 
         const activeSlide = document.querySelector('[data-activeSlide]');
         let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if(newIndex < 0) newIndex = slides.children.length = 0;
+        if(newIndex < 0) newIndex = slides.children.length - 1;
+        if(newIndex >= slides.length) newIndex = 0;
+        slides.children[newIndex] = dataset.active = true
 })
     })  
