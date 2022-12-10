6f8c4aba835e99e4448b2feda188dd63298f64b9
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-09 22:09:56 -0500

    script and html moved to root

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,7 @@ const Carousel = () => (
             <button className="carousel-button prev" data-carousel-button="prev">&#8249;</button>
             <button className="carousel-button next" data-carousel-button="next">&#8250;</button>
            <h1>Happiness Carousel</h1>
+           
             <ul data-slides>
                 <li className="slide" data-active> 
                 <img src={happyBalloons} alt="Festive Balloons"/>
