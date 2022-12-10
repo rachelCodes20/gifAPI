f8e4785f00d9deafc1f9e84b0dac7519d5d5dd26
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-09 21:27:09 -0500

    traverse button elements for carousel functionality

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const buttons = document.querySelector("[data-carousel-button]");
+console.log('activeSlide', buttons)
 buttons.forEach( (button, i) => {
     button.addEventListener('click', () => {
         button.dataset.carouselButton === 'prev' ? 1 : -1;
         const slides = button.closest("[data-carousel]")
-        .querySelector('[data-slides]')
-    })  
+        .querySelector('[data-slides]');
+
+        const activeSlide = document.querySelector('[data-activeSlide]');
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if(newIndex < 0) newIndex = slides.children.length = 0;
 })
+    })  
