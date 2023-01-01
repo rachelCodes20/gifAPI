bd5cb667e705365478d4ce0f8e112f753605f91a
Rachel Collins <spiritedndiligent@gmail.com>
2023-01-01 15:21:17 -0500

    formatting gitig additions

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -1,9 +1,18 @@
-const buttons = document.querySelector("[data-carousel-button]");
-buttons.forEach((button, i)=>{
-    button.addEventListener("click", ()=>{
-        button.dataset.carouselButton;
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
-    });
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // button.dataset.carouselButton === 'next' ? 1 : -1;
+    // const slides = button
+    // .closest('[data-carousel/hugo-ruiz-e2pVrE1PYzs-unsplash.6bb25e5b.jpg]')
+    // .querySelector('[data-slides]');
+    // const activeSlide = slides.querySelector('[data-activeSlide]');
+    // let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    // if(newIndex < 0) newIndex = slides.children.length - 1;
+    // if(newIndex >= slides.children.length) newIndex = 0;
+    // slides.children[newIndex] = dataset.active = true;
+    // delete activeSlide.dataset.active
+    console.log("clicked");
+  });
 });
 
 //# sourceMappingURL=index.672d4772.js.map
