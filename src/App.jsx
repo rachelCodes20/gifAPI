d737f9c4fdb0f28d335a3cc9b019cc3cfa408036
Rachel Collins <spiritedndiligent@gmail.com>
2023-01-02 18:07:22 -0500

    recode of carousel div/background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import React, { useState, useEffect } from "react";
 import GifContainer from "./GifContainer";
 import Carousel from "./Carousel";
 const axios = require("axios");
+import happyBalloons from "./Assets/madison-oren-uGP_6CAD-14-unsplash.jpg";
+import doggieBday from "./Assets/delaney-dawson-BXs8SjVelKs-unsplash.jpg";
+import confettiGirl from "./Assets/hugo-ruiz-e2pVrE1PYzs-unsplash.jpg";
+
+function handleClick() {
+}
 
 const App = (props) => {
   const [userInput, setUserInput] = useState("");
   const [gifResults, setGifResults] = useState([]);
   const [error, setError] = useState(false);
+  const slides = [
+      {url: happyBalloons, title: "Happy Ballons"},
+      {url: doggieBday, title: "Doggy Birthday"},
+      {url: confettiGirl, title: "Confetti Girl"}
+     ]
   const notEnoughResults = () => {
     return <h1>Your search has too few results </h1>;
   };
@@ -73,7 +84,7 @@ const App = (props) => {
         notEnoughResults={notEnoughResults}
         render={render}
       />
-      <Carousel />
+      <Carousel slides={slides} />
     </div>
   );
 };
---
+++ src/ImageSlider.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+const ImageSlider = ({ slides }) => {
+const [currentIndex, setCurrentIndex] = useState(0);
+return <div> 
+    <div style={{backgroundImage: `url(${slides[currentIndex].url})`}}> 
+
+    </div>
+</div>
+}
+
+export default ImageSlider;
